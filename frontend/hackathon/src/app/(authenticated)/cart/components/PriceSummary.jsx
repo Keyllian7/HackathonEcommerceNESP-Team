565a95e5fcfbbf4ea285a811e52ad1c792252f85
Cabral <thicabral2018@outlook.com>
2024-09-05 10:25:02 -0300

    feat: adionando o valor, imagem, nome dinamicamente

--- frontend/hackathon/src/app/(authenticated)/cart/components/PriceSummary.jsx
+++ frontend/hackathon/src/app/(authenticated)/cart/components/PriceSummary.jsx
@@ -1,9 +1,12 @@
-export const PriceSummary = () => {
+export const PriceSummary = ({ price, freight }) => {
+
+    const total = price + freight
+
     return (
         <div className="flex flex-col gap-2 bg-white rounded-xl p-2">
-            <span>Subtotal: R$ 34,90</span>
-            <span>Frete: R$ 5,00</span>
-            <span>Total: R$ 39,90</span>
+            <span>Subtotal: R$ {price.toFixed(2)}</span>
+            <span>Frete: R$ {freight.toFixed(2)}</span>
+            <span>Total: R$ {total.toFixed(2)}</span>
         </div>
     )
 }
